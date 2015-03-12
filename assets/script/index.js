38601f103b1a710af061c28ba6bc6ce9ac776895
Andy Hanson <andy.pj.hanson@gmail.com>
2015-03-11 23:51:23 -0700

    Better help

--- assets/script/index.js
+++ assets/script/index.js
@@ -3,5 +3,16 @@
 var
 	$ = require("jquery"),
 	Game = require("./Game")
+require("jquery.scrollto")
 
-$(function() { new Game() })
+$(function() {
+	new Game()
+
+	var about = $("#about")
+	about.hide()
+	$("#help").click(function() {
+		about.toggle()
+		if (about.is(":visible"))
+			$(window).scrollTo(about)
+	})
+})
